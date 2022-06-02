4ea183cc9202e6c023710c9ea1e2a37fe3f21caa
Duodu Randy <duodurandy19@gmail.com>
2022-06-02 22:07:26 +0000

    Improved CI by adding GitHub Action worflows for testing and building documentation

--- source/conf.py
+++ source/conf.py
@@ -13,13 +13,14 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
+from datetime import datetime
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'Protege OWL'
-copyright = '2022, Randy'
-author = 'Randy'
+project = 'Protégé OWL'
+copyright = f"{datetime.utcnow().year}, Michael DeBellis"
+author = 'Michael DeBellis'
 
 
 # -- General configuration ---------------------------------------------------
@@ -29,8 +30,8 @@ author = 'Randy'
 # ones.
 extensions = [
     "sphinx_copybutton",
-    "sphinx_design"
-    # "sphinx_immaterial",
+    "sphinx_design",
+    "sphinx_immaterial",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -47,9 +48,66 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_immaterial'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_css_files = ["custom.css"]
+
+
+html_sidebars = {
+    "**": ["about.html", "navigation.html", "searchbox.html"],
+}
+html_title = 'Protégé OWL'
+html_logo = '_static/media/onto-logo.svg'
+html_favicon = '_static/media/onto-favicon.svg'
+
+# material theme options (see theme.conf for more information)
+html_theme_options = {
+    "icon": {
+        "repo": "fontawesome/brands/github-square",
+    },
+    "site_url": "https://isolveit.github.io/protege_owl/",
+    "repo_url": "https://github.com/iSOLveIT/protege_owl",
+    "repo_name": "Protégé",
+    "repo_type": "github",
+    "edit_uri": "blob/main",
+    "globaltoc_collapse": True,
+    "features": [
+        # "navigation.expand",
+        # "navigation.tabs",
+        # "toc.integrate",
+        # "navigation.sections",
+        # "navigation.instant",
+        # "header.autohide",
+        "navigation.top",
+        # "navigation.tracking",
+        # "search.highlight",
+        "search.share",
+    ],
+    "palette": [
+        {
+            "media": "(prefers-color-scheme: light)",
+            "scheme": "default",
+            "primary": "blue",
+            "accent": "teal",
+            "toggle": {
+                "icon": "material/weather-night",
+                "name": "Switch to dark mode",
+            },
+        },
+        {
+            "media": "(prefers-color-scheme: dark)",
+            "scheme": "slate",
+            "primary": "blue",
+            "accent": "teal",
+            "toggle": {
+                "icon": "material/weather-sunny",
+                "name": "Switch to light mode",
+            },
+        },
+    ],
+    "toc_title_is_page_title": True,
+}
